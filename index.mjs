eb3b512f5cc80181a811dd749df3ecae3afdc462
adurodo1 <adurodo1@my.centennialcollege.ca>
2022-03-11 22:10:25 -0500

    extenmsion changed from .js to .mjs fixed- 16

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
  
-import routes from './routes/capstoneRoutes.mjs';
+import routes from './routes/capstoneRoutes';
 import  { Server} from "socket.io" 
 import http from 'http';
 import cors from 'cors';
