550b59770a9ba793f1ff5dfed2d152772db4b45e
adurodo1 <adurodo1@my.centennialcollege.ca>
2022-03-14 17:29:45 -0400

    functions and delegated to manage new attributes of adjusted models

--- controllers/parameterController.mjs
+++ controllers/parameterController.mjs
@@ -1,6 +1,7 @@
 import mongoose  from "mongoose";
 
 import {ParameterSchema} from '../models/parameterModel.mjs';
+import {alertSchema} from '../models/alertModel.mjs'
 
 import  {Emails} from "./emails.mjs";
 
@@ -11,11 +12,12 @@ import io from '../index.mjs'
 
 
 const Parameter= mongoose.model('Parameter',ParameterSchema);
+const Alerts= mongoose.model('ParameterAlert',ParameterSchema);
 
 export const addNewEntry =(req,res) =>{
-    let newPlayer = new Parameter(req.body);
+    let newParams = new Parameter(req.body);
 
-    newPlayer.save((err,param)=>{
+    newParams.save((err,param)=>{
         if(err){
             res.send(err)
         }
@@ -79,9 +81,21 @@ export const sendEmail =async (req,res) =>{
  
  var email = new Emails(req.body);
 
- //await email.send();
- io.emit('AlertEvent', {alert:req.body});//Emits the Alert
+ let newAlerts = new Alerts(req.body);
 
- console.log(req.body)
+ newAlerts.save((err,param)=>{
+     if(err){
+         res.send(err)
+     }
+     
+     console.log(param)
+     await io.emit('AlertEvent', {alert:param});//Emits the Alert
+    // res.json(param);
+ });
+
+ //await email.send();
+
+
+ console.log(param)
    
 }
